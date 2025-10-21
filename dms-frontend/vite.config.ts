cc7a40a140c4b0cace052f1177db6ddf99a725f3
Alexander Halsey <49983070+AlexanderHalsey@users.noreply.github.com>
2025-10-21 22:05:30 +0200

    fix for npm scripts

--- dms-frontend/vite.config.ts
+++ dms-frontend/vite.config.ts
@@ -1,15 +1,27 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
+import { defineConfig, PluginOption } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [vue(), vueDevTools()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
+export default defineConfig(async ({ command }) => {
+  const plugins: PluginOption[] = [vue()]
+
+  // Only add devtools in development (not during build)
+  if (command !== 'build') {
+    const { default: vueDevTools } = await import('vite-plugin-vue-devtools')
+    plugins.push(vueDevTools())
+  }
+
+  return {
+    plugins, // Use the plugins array we built above
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
+      },
     },
-  },
+    server: {
+      port: 8080,
+    },
+  }
 })
