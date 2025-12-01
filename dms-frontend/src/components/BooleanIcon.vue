5c44b12444f79f6143cc56ce3f335c9c366c75a3
Alexander Halsey <49983070+AlexanderHalsey@users.noreply.github.com>
2025-12-01 16:13:29 +0100

    add BooleanIcon component

---
+++ dms-frontend/src/components/BooleanIcon.vue
@@ -0,0 +1,9 @@
+<template>
+  <QIcon :name="value ? 'check_circle' : 'cancel'" :color="value ? 'green' : 'red'" size="sm" />
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  value: boolean
+}>()
+</script>
