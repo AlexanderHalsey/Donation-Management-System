e95ff86cc42a046c457b98351af761b498061d3d
Alexander Halsey <49983070+AlexanderHalsey@users.noreply.github.com>
2026-03-09 22:18:53 +0100

    update organisation ref list on organisation create / update / delete

--- dms-frontend/src/stores/organisation.ts
+++ dms-frontend/src/stores/organisation.ts
@@ -1,6 +1,10 @@
 import { ref } from 'vue'
 import { defineStore } from 'pinia'
 
+import { pick } from 'es-toolkit'
+
+import { useOrganisationListStore } from './organisationList'
+
 import {
   disableOrganisation,
   getOrganisation,
@@ -12,6 +16,8 @@ import type { Organisation } from '@shared/models'
 import type { OrganisationFormData } from '@/features/organisations'
 
 export const useOrganisationStore = defineStore('organisation', () => {
+  const organisationListStore = useOrganisationListStore()
+
   const organisation = ref<Organisation>()
 
   const fetchOrganisation = async (organisationId: string) => {
@@ -20,15 +26,35 @@ export const useOrganisationStore = defineStore('organisation', () => {
 
   const createOrganisation = async (formData: OrganisationFormData) => {
     organisation.value = await postOrganisation(formData)
+    organisationListStore.organisationRefList.push(
+      pick(organisation.value, ['id', 'name', 'isDisabled', 'isTaxReceiptEnabled']),
+    )
   }
 
   const updateOrganisation = async (organisationId: string, formData: OrganisationFormData) => {
     organisation.value = await putOrganisation(organisationId, formData)
+    const index = organisationListStore.organisationRefList.findIndex(
+      (organisation) => organisation.id === organisationId,
+    )
+    if (index !== -1) {
+      organisationListStore.organisationRefList[index] = pick(organisation.value, [
+        'id',
+        'name',
+        'isDisabled',
+        'isTaxReceiptEnabled',
+      ])
+    }
   }
 
   const deleteOrganisation = async (organisationId: string) => {
     await disableOrganisation(organisationId)
     organisation.value = undefined
+    const index = organisationListStore.organisationRefList.findIndex(
+      (organisation) => organisation.id === organisationId,
+    )
+    if (index !== -1) {
+      organisationListStore.organisationRefList.splice(index, 1)
+    }
   }
 
   return {
